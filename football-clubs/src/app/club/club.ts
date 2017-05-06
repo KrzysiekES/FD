c970d7f1d0f7c313c0dcfdba7ec2ae2571977943
Anna Przeździak <Anna Przedziak>
2017-05-06 11:33:00 +0200

    single club component wip, routing

---
+++ football-clubs/src/app/club/club.ts
@@ -0,0 +1,19 @@
+export class Club {
+
+    name: string;
+    city: string;
+    coach: string;
+    ground: string;
+    league: string;
+    capacity: number;
+
+    constructor(name, city, coach, ground, league, capacity){
+        this.name = name;
+        this.city = city;
+        this.coach = coach;
+        this.ground = ground;
+        this.league = league;
+        this.capacity = capacity;
+    }
+    
+}
---
+++ football-clubs/src/app/club/single-club/single-club.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-single-club',
+  templateUrl: './single-club.component.html',
+  styleUrls: ['./single-club.component.css']
+})
+export class SingleClubComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
