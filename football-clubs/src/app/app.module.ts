1fc991700da9092a9c01fc927eb2678df147f365
Krzysztof Stelmach <krzysiekes.pol@gmail.com>
2017-05-06 11:33:00 +0200

    single club component wip, routing

--- football-clubs/src/app/app.module.ts
+++ football-clubs/src/app/app.module.ts
@@ -2,17 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { SingleClubComponent } from './club/single-club/single-club.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SingleClubComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      { path: 'home', component: SingleClubComponent },
+       { path: '', redirectTo: 'home', pathMatch: 'full' },
+       { path: '**', redirectTo: 'home', pathMatch: 'full' }
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
