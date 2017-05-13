8fe87613d7151adc6fafe23a6118fb7370a4921d
Krzysztof Stelmach <krzysiekes.pol@gmail.com>
2017-05-13 22:46:39 +0200

    addClub method in clubs service

--- football-clubs/src/app/club/create-club/create-club.component.ts
+++ football-clubs/src/app/club/create-club/create-club.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ClubsService } from './../clubs.service';
+
 
 @Component({
   selector: 'app-create-club',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateClubComponent implements OnInit {
 
-  constructor() { }
+  constructor(private clubsService: ClubsService) { }
 
   ngOnInit() {
   }
