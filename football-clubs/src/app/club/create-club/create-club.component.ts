7ec3b8d5ca387b86151cd230fec81a799fe04b8a
KrzysiekES <krzysiekes.pol@gmail.com>
2017-06-01 19:57:55 +0200

    Example of validators for creating Club

--- football-clubs/src/app/club/create-club/create-club.component.ts
+++ football-clubs/src/app/club/create-club/create-club.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit, Input, Output } from '@angular/core';
 import { ClubsService } from './../clubs.service';
 import { IClub } from './../iclub';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-club',
   templateUrl: './create-club.component.html',
-  styleUrls: ['./create-club.component.css']
+  styleUrls: ['./create-club.component.scss']
 })
 export class CreateClubComponent implements OnInit {
 
@@ -15,27 +15,42 @@ export class CreateClubComponent implements OnInit {
 
   clubForm: FormGroup;
 
-  ngOnInit() {
-    let name = new FormControl();
-    let coach = new FormControl();
-    let city = new FormControl();
-    let ground = new FormControl();
-    let league = new FormControl();
-    let capacity = new FormControl();
+  private name: FormControl;
+  private coach: FormControl;
+  private city: FormControl;
+  private ground: FormControl;
+  private league: FormControl;
+  private capacity: FormControl;
 
-    this.clubForm = new FormGroup( {
-        name: name,
-        coach: coach,
-        city: city,
-        ground: ground,
-        league: league,
-        capacity: capacity
+  ngOnInit() {
+    this.name = new FormControl('', [Validators.required, Validators.minLength(5)]);
+    this.city = new FormControl('', [Validators.required, Validators.minLength(5)]);
+    this.coach = new FormControl('');
+    this.ground = new FormControl('');
+    this.league = new FormControl('');
+    this.capacity = new FormControl('');
+
+    this.clubForm = new FormGroup({
+      name: this.name,
+      coach: this.coach,
+      city: this.city,
+      ground: this.ground,
+      league: this.league,
+      capacity: this.capacity
     });
   }
 
-    addClub(values): void {
-      this.clubsService.addClub(values);
-      console.log(values);
-    }
+  validateName() {
+    return !this.name.valid;
+  }
+
+  validateCity() {
+    return !this.city.valid;
+  }
+
+  addClub(values): void {
+    this.clubsService.addClub(values);
+    console.log(values);
+  }
 
 }
